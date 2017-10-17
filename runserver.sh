b691e9557401aae1b451b2407c1174b814b84437
ReactiveXYZ-Dev <Jackie@Jackies-MacBook-Pro.local>
2017-10-16 22:12:01 -0400

    Updated runserver

--- runserver.sh
+++ runserver.sh
@@ -2,4 +2,4 @@
 sudo kill -9 $(ps aux | grep -e uwsgi | awk '{ print $2 }')
 
 # reload server
-uwsgi --module web.wsgi --master --socket --enable-threads /var/www/perseus/perseus.sock --chmod-socket=666 
+uwsgi --module=web.wsgi  --processes=3 --http=127.0.0.1:8003 --enable-threads --reload-mercy=1 --worker-reload-mercy=1
